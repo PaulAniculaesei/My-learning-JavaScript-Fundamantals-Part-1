af0ee73f665dec64a13b65319d084eb399ddb49f
Paul Aniculaesei <23344248+PaulAniculaesei@users.noreply.github.com>
2022-01-21 15:47:08 +0200

    Add files via upload

---
+++ assignments.js
@@ -0,0 +1,49 @@
+/*
+const country = 'Romania';
+const continent = "Europe";
+let population = 17000000;
+
+console.log(country);
+console.log(continent);
+console.log(population);
+
+
+let isRomania = true;
+const language = 'romanian';
+
+
+console.log(typeof isRomania);
+console.log(typeof population);
+console.log(typeof country);
+console.log(typeof language);
+//language = 'chinese';
+
+const halfPopulation = population / 2;
+population++;
+console.log(population);
+
+const finland = 6000000;
+console.log(country > finland);
+
+const maxCountry = 33000000;
+console.log(country < maxCountry);
+const description = country + " is in " + continent + ',and its ' + population + ' people speak ' + language;
+console.log(description);
+console.log(`${country} is in ${continent}, and it's ${population} people speak ${language}`);
+console.log(`Ha!`);
+
+
+
+
+if (population > 33000000) {
+    console.log(`${country}'s population is above the average `);
+} else {
+    console.log(`${country}'s population is 17000000 milion below average`);
+}
+
+*/
+4;
+617
+23
+console.log("123" < 57);//false
+1143
---
+++ script.js
@@ -0,0 +1,221 @@
+/*
+let js = "amazing";
+console.log(40 + 8 + 15);
+console.log('Jonas');
+console.log(23);
+let firstName = 'Sara-Amelia';
+console.log(firstName);
+console.log(firstName);
+// leave it alone
+console.log(firstName);
+
+
+
+let _3years = 3;
+*/
+
+/*
+let javascriptIsFun = false;
+console.log(javascriptIsFun);
+console.log(typeof true);
+console.log(typeof 23);
+
+javascriptIsFun = "YES!";
+console.log(javascriptIsFun);
+
+let year;
+console.log(year);
+console.log(typeof year);
+
+
+let age = 30;
+age = 31;
+
+
+const birthYear = 1991;
+//birthYear = 1990;
+
+//const job;
+
+var job = "programmer";
+
+tob = 'teacher';
+
+lastName = 'Aniculaesei';
+console.log(lastName);
+
+
+
+
+const now = 2037;
+const agePaul = now - 1991;
+const ageSaraAmelia = now - 2018;
+console.log(agePaul, ageSaraAmelia);
+
+
+console.log(agePaul * 2, agePaul / 10, 2 ** 3);
+//2**3 means 2 to the power of 3 = 2*2*2
+
+
+
+const firstName = 'Paul';
+const lastName = 'Aniculaesei';
+console.log(firstName + ' ' + lastName);
+
+
+
+
+//math operators
+const now = 2037;
+const agePaul = now - 1991;
+const ageSaraAmelia = now - 2018;
+console.log(agePaul, ageSaraAmelia);
+
+
+//Assignment operators
+console.log(agePaul * 2, agePaul / 10, 2 ** 3);
+
+const firstName = "Paul";
+const lastName = 'Aniculaesei'
+console.log(firstName + ' ' + lastName);
+
+let x = 10 + 5;//15
+x += 10;
+x *= 4;
+x++;
+x--;
+x--;
+
+console.log(x);
+
+
+//Comparison operators
+
+console.log(agePaul > ageSaraAmelia);
+console.log(ageSaraAmelia >= 18);
+
+const isFullAge = ageSaraAmelia >= 18
+
+console.log(now - 1991 > now - 2018);
+
+
+
+const now = 2037;
+const agePaul = now - 1991;
+const ageSaraAmelia = now - 2018;
+console.log(agePaul, ageSaraAmelia);
+
+console.log(now - 1991 > now - 2018);
+
+console.log(25 - 10 - 5);
+
+let x, y;
+x = y = 25 - 10 - 5;//x = y = 10
+console.log(x, y);
+
+
+const averageAge = (agePaul + ageSaraAmelia) / 2;
+console.log(agePaul, ageSaraAmelia, averageAge);
+
+
+const massMark = 95;
+const heightMark = 1.88;
+const massJohn = 85;
+const heightJohn = 1.76;
+
+let bmiMark = massMark / (heightMark * heightMark);
+console.log(bmiMark);
+
+let bmiJohn = massJohn / (heightJohn * heightJohn);
+console.log(bmiJohn);
+
+let markHigherBMI = (bmiMark > bmiJohn);
+console.log(markHigherBMI);
+
+
+const firstName = "Paul";
+const job = "musician";
+const birthYear = 1985;
+const year = 2037;
+const paul = "I'm " + firstName + ',a ' + (year - birthYear) + ' years old ' + job + " !";
+console.log(paul);
+
+const paulNew = `I'm ${firstName}, a ${year - birthYear} years old ${job}!`;
+
+console.log(paulNew);
+
+console.log(`Szia magyar fiu...!!!`);
+
+console.log(`Szia
+Hogy vagy?
+Jol Vagy?`);
+
+
+
+
+const age = 17;
+
+
+if (age >= 18) {
+    console.log(`You can take the license!🏎🏎`);
+} else {
+    const yearsLeft = 18 - age;
+    console.log(`Wait more time!You have only ${yearsLeft} years leaft!`);
+}
+
+
+const birthYear = 2022;
+let century;
+if (birthYear <= 2000) {
+    century = 20;
+} else {
+    century = 21;
+}
+console.log(century);
+
+
+
+const massMark = 95;
+const heightMark = 1.88;
+const massJohn = 85;
+const heightJohn = 1.76;
+
+let bmiMark = massMark / (heightMark * heightMark);
+console.log(bmiMark);
+
+let bmiJohn = massJohn / (heightJohn * heightJohn);
+console.log(bmiJohn);
+
+let markHigherBMI = (bmiMark > bmiJohn);
+console.log(markHigherBMI);
+
+
+if (bmiMark > bmiJohn) {
+    console.log(`Marks's BMI (${bmiMark}) is higher than John's (${bmiJohn})`);
+} else {
+    console.log(`John's BMI (${bmiJohn}) is higher than Mark's (${bmiMark})`);
+}
+
+
+
+
+const inputYear = "1991";
+console.log(Number(inputYear), inputYear);
+console.log(Number(inputYear) + 18);
+
+
+console.log(Number("Jonas"));
+
+console.log(typeof NaN);
+
+
+console.log(String(23), 23);
+
+
+*/
+
+console.log(`I am 23 years old`);
+
+console.log("23" - "10 " + 3);
+
+console.log("23" / 3);
